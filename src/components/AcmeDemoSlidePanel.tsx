53905e1caaadc70c8161b1c139538f330356d951
okamniev <alexander.kamnev@gmail.com>
2023-12-26 10:54:56 +0100

    Create AcmeDemoSLidePanel component

---
+++ src/components/AcmeDemoSlidePanel.tsx
@@ -0,0 +1,25 @@
+import React, { FC, ReactElement } from 'react';
+import { Box, Modal } from '@mui/material';
+
+import './AcmeDemoSlidePanel.css';
+
+type AcmeDemoSLidePanelType = {
+    isOpen: boolean,
+    children: ReactElement,
+    onClose: () => void,
+};
+
+const AcmeDemoSlidePanel:FC<AcmeDemoSLidePanelType> = ({isOpen, onClose, children}) => {
+  return (
+      <Modal
+          open={isOpen}
+          onClose={onClose}
+      >
+          <div className={'AcmeDemoSlidePanel'} >
+              { children }
+          </div>
+      </Modal>
+  )
+}
+
+export default AcmeDemoSlidePanel;
